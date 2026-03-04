7b23e687c60b28f9b1545b6818deb03df53969cc
char-mac12 <char.emily.mac@gmail.com>
2026-03-04 00:36:06 +0000

    Create CSV reporter

---
+++ reports/csv_reporter.py
@@ -0,0 +1,37 @@
+import csv
+import json
+from datetime import datetime
+
+from attacks.attack_result import AttackResult
+from core.config import LogLevel
+from core.logger import log
+from core.registries import REPORTER_REGISTRY, register
+from reports.base import BaseReporter
+from pathlib import Path
+
+@register(REPORTER_REGISTRY, "csv")
+class CsvReporter(BaseReporter):
+    def __init__(self, config=None):
+        config = config or {}
+        self.output_dir = Path(config.get("output_dir", "outputs/csv"))
+        self.output_dir.mkdir(parents=True, exist_ok=True)
+        log(f"[Reporter] CsvReporter initialized, saving to {self.output_dir}", LogLevel.VERBOSE)
+
+    def report(self, result: AttackResult, run_config=None):
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+        filepath = self.output_dir / f"Privacy_Audit_Report_{timestamp}.csv"
+
+        report_data = {
+            "timestamp": timestamp,
+            "attack_name": result.attack_name,
+            "model_name": result.model_name,
+            "dataset_name": result.dataset_name,
+            "attack_duration": result.attack_duration
+        }
+
+        report_data.update(result.metrics)
+        
+        with open(filepath, "w", newline="") as f:
+            writer = csv.DictWriter(f, fieldnames=report_data.keys())
+            writer.writeheader()
+            writer.writerow(report_data)
