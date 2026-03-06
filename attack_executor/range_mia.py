ce0fa8ecddaf4e28f1d4b6f3cbb08848013c403f
char-mac12 <char.emily.mac@gmail.com>
2026-03-06 23:41:28 +0000

    Range MIA

--- attack_executor/range_mia.py
+++ attack_executor/range_mia.py
@@ -1,31 +1,41 @@
 import numpy as np
+from transformers import AutoTokenizer, AutoModelForMaskedLM
+import torch
 
 from core.registries import register, ATTACK_REGISTRY
 from attack_executor.base import BaseAttack
 from core.logger import log, LogLevel
-from attack_executor.range_sampler import RangeSampler
+from attack_executor.range_sample_word_replace import sample_word_replace
 
 @register(ATTACK_REGISTRY, "range-mia")
 class RangeMIA(BaseAttack):
     display_name = "Range MIA"
-    paraphrase_methods = ["word_replace", "prefix"]
 
     def __init__(self, config=None):
         cfg = config or {}
-        log(f"")
+        log(f"[Attack] {self.display_name} initialised", LogLevel.VERBOSE)
 
         self.base_attack_id = cfg.get("base_attack", "loss-based-mia")
         self.sample_size = cfg.get("sample_size", 10)
-        self.paraphrase_method = cfg.get("paraphrase_method", "word_replace")
-        self.config = cfg
 
+        # neighbourhood sample config
+        self.num_masks = cfg.get("num_masks", 1)
+        self.mask_model = cfg.get("mask_model", "bert-base-uncased")
+        device_config = cfg.get("device", "cuda")
+        self.device = torch.device(device_config if torch.cuda.is_available() else "cpu")
+
+        self.mlm_model = AutoModelForMaskedLM.from_pretrained(self.mask_model).to(self.device)
+        self.mlm_tokenizer = AutoTokenizer.from_pretrained(self.mask_model)
+        if self.mlm_tokenizer.pad_token is None:
+            self.mlm_tokenizer.pad_token = self.mlm_tokenizer.eos_token
+
+        # base attack config
         base_attack_cls = ATTACK_REGISTRY[self.base_attack_id]
         self.higher_is_member = base_attack_cls.higher_is_member
 
-        self._sampler = RangeSampler(range_fn=self.paraphrase_method,
-                                     sample_size=self.sample_size,
-                                     config=self.config)
-
+        # trimmed average config
+        self.trim_start = cfg.get("trim_start", 0.1)
+        self.trim_end = cfg.get("trim_end", 0.9)
         
     def score(self, model, samples):
         scores = []
@@ -38,16 +48,29 @@ class RangeMIA(BaseAttack):
 
             neighbour_scores = base_attack.score(model, neighbourhood)
             
-            range_score = float(np.mean(neighbour_scores))
+            range_score = self._trimmed_average(neighbour_scores)
             scores.append(range_score)
 
         return scores
-    
-    def _generate_neighbourhood(self, sample):
-        if self.paraphrase_method not in self.paraphrase_methods:
-            raise NotImplementedError(
-                f"Range MIA currently only supports {self.paraphrase_methods}. "
-                f"Requested method: {self.paraphrase_method}."
-            )
 
-        return self._sampler.sample(sample)
+    def _generate_neighbourhood(self, sample):
+        return sample_word_replace(
+                sample, 
+                self.mlm_model, 
+                self.mlm_tokenizer, 
+                self.num_masks, 
+                self.sample_size, 
+                self.device
+            )
+    
+    def _trimmed_average(self, scores):
+        scores = np.array(scores)
+        scores = np.sort(scores)
+
+        n = len(scores)
+        start_idx = int(np.ceil(self.trim_start * n))
+        end_idx = int(np.floor(self.trim_end * n))
+
+        trimmed_scores = scores[start_idx:end_idx]
+
+        return float(np.mean(trimmed_scores))
